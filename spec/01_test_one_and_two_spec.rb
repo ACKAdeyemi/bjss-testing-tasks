3ea043565c260d40b9ee4dba1b062c10aaa0cad3
ACKAdeyemi <akola-adeyemi@spartaglobal.com>
2018-08-01 00:40:03 +0100

    updated basic setup

--- spec/01_test_one_and_two_spec.rb
+++ spec/01_test_one_and_two_spec.rb
@@ -12,7 +12,6 @@ describe "Testing two examples from the-internet app" do
       @the_internet_site.the_internet_homepage.visit_home_page
       @the_internet_site.the_internet_homepage.click_challenging_dom
       expect(@the_internet_site.helper_methods.check_page_content('Challenging DOM')).to eq true
-
       @the_internet_site.helper_methods.wait(1)
     end
 
@@ -24,7 +23,6 @@ describe "Testing two examples from the-internet app" do
       @the_internet_site.the_internet_homepage.visit_home_page
       @the_internet_site.the_internet_homepage.click_dynamic_loading
       expect(@the_internet_site.helper_methods.check_page_content('Dynamically Loaded Page Elements')).to eq true
-
       @the_internet_site.the_internet_dynamic_loading.click_example_two
       expect(@the_internet_site.helper_methods.check_page_content('Example 2: Element rendered after the fact')).to eq true
       @the_internet_site.helper_methods.wait(3)
